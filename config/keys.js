1c79ba7b21b4d6d7c445324f505c13b4ae9a0f4f
Mykola Kurlyak <mykola.kurlyak@visiquate.com>
2023-03-05 17:02:13 +0200

    first commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://localhost:27017'
+}
+// mongodb+srv://koka81000:30498741@cluster0.pcvtgsd.mongodb.net/?retryWrites=true&w=majority
+// MEAN BD (Pet Analytic Project)
