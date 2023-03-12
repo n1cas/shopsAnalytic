f0db8b2985ea4899bf899845d336c587c78d9184
Mykola Kurlyak <mykola.kurlyak@visiquate.com>
2023-03-12 23:00:01 +0200

    added upload file logic

--- controllers/category.js
+++ controllers/category.js
@@ -1,22 +1,71 @@
-module.exports.getAll = function(req, res) {
-    res.json({
-        message: 'categoties'
-    })
+const Category = require("../models/Category");
+const Position = require("../models/Position");
+const errorHandler = require("../utils/errorHandler");
 
-}
+module.exports.getAll = async function (req, res) {
+  try {
+    const categories = await Category.find({ user: req.user.id });
 
-module.exports.getById = function(req, res) {
+    res.status(200).json(categories);
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
 
-}
+module.exports.getById = async function (req, res) {
+  try {
+    const category = await Category.findById(req.params.id);
 
-module.exports.remove = function(req, res) {
+    res.status(200).json(category);
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
 
-}
+module.exports.remove = async function (req, res) {
+  try {
+    await Category.remove({ _id: req.params.id });
+    await Position.remove({ category: req.params.id });
 
-module.exports.create = function(req, res) {
+    res.status(200).json({ message: "Category was removed" });
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
 
-}
+module.exports.create = async function (req, res) {
+  const category = new Category({
+    name: req.body.name,
+    user: req.user.id,
+    imageSrc: req.file ? req.file.path : "",
+  });
 
-module.exports.update = function(req, res) {
+  try {
+    await category.save();
+    res.status(200).json(category);
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
 
-}
+module.exports.update = async function (req, res) {
+  const updated = {
+    name: req.body.name,
+  };
+
+  if (req.file) {
+    updated.imageSrc = req.file.path;
+  }
+
+  try {
+    const category = await Category.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: updated },
+      { new: true }
+    );
+
+    res.status(200).json(category);
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
