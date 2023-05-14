8a86f1ba883c230a22d6ad1a6b783c6cb997ef46
Mykola Kurlyak <mykola.kurlyak@visiquate.com>
2023-05-14 22:24:35 +0300

    added order controllers

--- routes/order.js
+++ routes/order.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const controller = require('../controllers/order')
-const router = express.Router()
+const express = require("express");
+const controller = require("../controllers/order");
+const passport = require("passport");
+const router = express.Router();
 
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  controller.getAll
+);
 
-router.get('/', controller.getAll)
-router.post('/', controller.create)
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  controller.create
+);
 
-
-module.exports = router
+module.exports = router;
